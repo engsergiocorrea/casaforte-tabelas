8b59ef8ee31db611865aec48e0568fd06be8d305
engsergiocorrea <engsergiocorrea@gmail.com>
2026-05-28 10:33:04 -0300

    ignora .next

--- .next/static/chunks/app/(public)/page-dd6c74b6aad1d8b2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[952],{76750:function(e,n,s){Promise.resolve().then(s.t.bind(s,65878,23)),Promise.resolve().then(s.t.bind(s,72972,23))}},function(e){e.O(0,[972,878,971,117,744],function(){return e(e.s=76750)}),_N_E=e.O()}]);
